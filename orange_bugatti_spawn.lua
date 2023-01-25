e334eddd9bc63a0884cb0ee8a790fb44e603b97d
OrangeBugatti <123387724+OrangeBugatti@users.noreply.github.com>
2023-01-25 11:18:10 -0500

    Create orange_bugatti_spawn.lua

---
+++ orange_bugatti_spawn.lua
@@ -0,0 +1,105 @@
+-- Orange Bugattis custom spawn menu
+local ENT = {}
+
+-- Set the entity to inherit from the base entity class
+ENT.Type = "anim"
+
+-- Set the class name
+ENT.ClassName = "spawner"
+
+-- Define the properties and behaviors of the entity
+function ENT:Initialize()
+    -- code to initialize the entity
+end
+
+function ENT:Think()
+    -- code for the entity's logic
+end
+
+-- Function to open the vehicle spawn menu
+function ENT:OpenVehicleMenu(ply)
+    -- Check if the player has the necessary permissions
+    if not table.HasValue({"superadmin", "admin"}, ply:GetUserGroup()) then 
+        ply:ChatPrint("You do not have permission to use this spawner.")
+        return 
+    end
+    -- Open the menu
+    -- You would need to create a separate function to actually spawn the vehicles
+    -- and call it from the menu
+    ply:ConCommand("open_vehicle_spawn_menu")
+end
+
+-- Check for "e" button press on the entity
+hook.Add("PlayerUse", "CheckEntityPress", function(ply, ent)
+    if ent == ent and ply:KeyDown(IN_USE) then
+        ent:OpenVehicleMenu(ply)
+    end
+end)
+
+
+-- Register the entity with the engine
+scripted_ents.Register(ENT, ENT.ClassName, true)
+
+-- Add your entity to the spawn menu
+list.Set( "SpawnableEntities", "spawner", { "models/props_c17/furnitureStove001a.mdl", "spawner" } )
+
+-- Create a new category for your custom entity
+list.SetCategory("Vehicle Spawner", "spawner", "Vehicle Spawner")
+
+-- Function to open the vehicle spawn menu
+function ENT:OpenVehicleMenu(ply)
+    -- Check if the player has the necessary permissions
+    if not table.HasValue({"superadmin", "admin"}, ply:GetUserGroup()) then 
+        ply:ChatPrint("You do not have permission to use this spawner.")
+-- Function to spawn a vehicle
+function ENT:SpawnVehicle(ply, vehicle)
+    -- Check if the player has the necessary permissions
+    if not table.HasValue({"superadmin", "admin"}, ply:GetUserGroup()) then 
+        ply:ChatPrint("You do not have permission to use this spawner.")
+        return 
+    end
+
+    -- Spawn the vehicle
+    ply:ConCommand("gm_spawn " .. vehicle)
+end
+
+
+-- Create the menu
+local function CreateVehicleSpawnMenu(ply)
+    local frame = vgui.Create("DFrame")
+    frame:SetSize(300, 300)
+    frame:Center()
+    frame:SetTitle("Vehicle Spawn Menu")
+    frame:MakePopup()
+
+    -- Create a button for the buggy
+    local button = vgui.Create("DButton", frame)
+    button:SetText("Spawn Buggy")
+    button:SetPos(25, 50)
+    button:SetSize(250, 50)
+    button.DoClick = function()
+        ent:SpawnVehicle(ply, "buggy")
+    end
+
+    -- Create a button for the sports car
+    local button2 = vgui.Create("DButton", frame)
+    button2:SetText("Spawn Sports Car")
+    button2:SetPos(25, 110)
+    button2:SetSize(250, 50)
+    button2.DoClick = function()
+        ent:SpawnVehicle(ply, "sportscar")
+    end
+end
+
+
+-- Function to spawn a vehicle
+function ENT:SpawnVehicle(ply, vehicle)
+    -- Check if the player has the necessary permissions
+    if not table.HasValue({"superadmin", "admin"}, ply:GetUserGroup()) then 
+        ply:ChatPrint("You do not have permission to use this spawner.")
+        return 
+    end
+
+    -- Spawn the vehicle
+    ply:ConCommand("gm_spawn " .. vehicle)
+end
