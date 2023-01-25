ba2dc0204b591922e78e7cdfc1b7d378c4b36ba7
OrangeBugatti <123387724+OrangeBugatti@users.noreply.github.com>
2023-01-25 12:28:30 -0500

    Create shared.lua

---
+++ shared.lua
@@ -0,0 +1,9 @@
+ENT.Base = "base_anim"
+ENT.Type = "ai"
+ENT.Base = "base_gmodentity"
+
+ENT.PrintName = "Orange's Menu"
+ENT.AuthorName = "Orange Bugatti"
+ENT.Category = "[Orange Menu]"
+ENT.Spawnable = true
+ENT.RenderGroup = RENDERGROUP_BOTH
